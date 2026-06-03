ff32148f9bd688f8227578dfab655ef1e9ef2588
Gustavo Lucas <gustavolucasdev@gmail.com>
2026-06-03 16:43:51 -0300

    Update interface

--- src/main/java/com/gustavolucasdev/Program.java
+++ src/main/java/com/gustavolucasdev/Program.java
@@ -17,9 +17,13 @@ public class Program {
         Scanner sc = new Scanner(System.in);
 
         int opcao;
-        System.out.println("Bem vindo ao cadastro de veículos!");
+        System.out.println("================================================================");
+        System.out.println("|              Bem vindo ao cadastro de veículos!              |");
+        System.out.println("================================================================");
         do {
+            System.out.println("                    O que você deseja fazer?                    ");
             System.out.println("1-Cadastrar, 2-Listar, 3-Buscar por placa, 4-Remover, 5-Sair");
+            System.out.print("Insira aqui: ");
             opcao = sc.nextInt();
 
             switch (opcao) {
@@ -45,13 +49,15 @@ public class Program {
 
     public static void cadastrarVeiculo() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Informe a placa: ");
+        System.out.println("================================================================");
+        System.out.println("                Área de cadastramento de veículos               ");
+        System.out.print("Informe a placa: ");
         String placa = sc.nextLine();
-        System.out.println("Informe a marca: ");
+        System.out.print("Informe a marca: ");
         String marca = sc.nextLine();
-        System.out.println("Informe o modelo: ");
+        System.out.print("Informe o modelo: ");
         String modelo = sc.nextLine();
-        System.out.println("Informe o ano: ");
+        System.out.print("Informe o ano: ");
         int ano = sc.nextInt();
         Veiculos veiculo = new Veiculos(
                 placa,
@@ -61,39 +67,55 @@ public class Program {
         );
         service.cadastrar(veiculo);
         System.out.println("Veículo cadastrado com sucesso!");
+        System.out.println("================================================================");
+        System.out.println();
     }
 
     public static void listarVeiculos() {
 
         List<Veiculos> veiculos = service.listar();
-
+        System.out.println("===============================================================================");
+        System.out.println("                    Área de listagem de veículos cadastrados                   ");
         if (veiculos.isEmpty()) {
             System.out.println("Nenhum veículo cadastrado.");
+            System.out.println("===============================================================================");
+            System.out.println();
             return;
         }
 
         for (Veiculos v : veiculos) {
+            System.out.println("-------------------------------------------------------------------------------");
             System.out.println(v);
+            System.out.println("-------------------------------------------------------------------------------");
         }
+        System.out.println("===============================================================================");
+        System.out.println();
     }
 
     public static void buscarVeiculo() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Informe a placa:");
+        System.out.println("================================================================");
+        System.out.println("               Área de busca de veículo por placa               ");
+        System.out.print("Informe a placa: ");
         String placa = sc.next();
 
         Veiculos veiculo = service.buscarPlaca(placa);
 
         if (veiculo != null) {
             System.out.println(veiculo);
+            System.out.println("================================================================");
         } else {
             System.out.println("Veículo não encontrado.");
+            System.out.println("================================================================");
         }
+        System.out.println();
     }
 
     public static void removerVeiculo() {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Informe a placa:");
+        System.out.println("================================================================");
+        System.out.println("             Área de remoção de veículos cadastrados            ");
+        System.out.print("Informe a placa: ");
         String placa = sc.next();
 
         boolean removido = service.removerPorPlaca(placa);
@@ -103,6 +125,8 @@ public class Program {
         } else {
             System.out.println("Veículo não encontrado.");
         }
+        System.out.println("================================================================");
+        System.out.println();
     }
 
 }
